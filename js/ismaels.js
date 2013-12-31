b54aa0303fe6ba4881b0f70e59d6ccc16746443a
HenryGBC <henry.bravo89@gmail.com>
2013-12-31 17:26:46 -0430

    Estilo a etiquetas de Ingredientes y Precio

--- js/ismaels.js
+++ js/ismaels.js
@@ -1,11 +1,12 @@
 $(function(){
-		
+		var id = '#plato_pedido';
+		var idPlato;
 		$('.anadir').on('click', function(ev){
 			ev.preventDefault();
 			var plato = $('input[name=tituloplato]').val();
 			var cantidad = $('input[name=cantidad]').val();
 			var clase = "plato"
-			var template = '<article id ="plato_pedido" class="'+clase+'"> \
+			var template = '<article id ="'+plato+'" class="'+clase+'"> \
 							<a href="#"> \
 								<span class="nomplato"> \
 									<strong>'+plato+'</strong> \
@@ -17,16 +18,18 @@ $(function(){
 								</span> \
 							</a> \
 						</article>';
+			
 			$('.pedido').prepend($(template));
+	
+			
 			
 		});
 
-		$('.plato').on('click', function(ev){
-			ev.preventDefault();
-			var id = $(".pedido").attr("id");
-			alert(id);
 
+		$('#Hallaca').on('click', function(ev){
+			debugger;
 		});
+
 
 			
 			
